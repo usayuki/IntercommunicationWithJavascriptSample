3530365f8b1084c96f152680e33b6245d260e445
usayuki <masapp.01a@gmail.com>
2019-06-17 10:30:33 +0900

    initial commit

---
+++ IntercommunicationWithJavascript/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntercommunicationWithJavascript
+//
+//  Created by 石川 雅之 on 2019/06/17.
+//  Copyright © 2019 usayuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
